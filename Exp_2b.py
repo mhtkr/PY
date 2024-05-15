6848b87216883128dc0e8a658c2b9b809fd3d321
M O H I T  K U M A R <141761768+mhtkr@users.noreply.github.com>
2024-05-15 22:49:56 +0530

    Create Exp_2b.py

---
+++ Exp_2b.py
@@ -0,0 +1,18 @@
+info = {}
+
+info['name'] = input("Enter Your Name - ")
+
+info['age'] = int(input("Enter your age -"))
+
+if info['age'] >= 18:
+
+  print(info['name'], "you are eligible")
+
+
+else:
+
+  print(info['name'], "you are not eligible")
+
+dic = {'rohan':20, 'Mohit':10}
+
+print(dic['Mohit'])
