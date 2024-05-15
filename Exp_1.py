babd9c7bf8926360c07f3b5b41ea2cb396fd2ea2
M O H I T  K U M A R <141761768+mhtkr@users.noreply.github.com>
2024-05-15 22:48:34 +0530

    Create Exp_1.py

---
+++ Exp_1.py
@@ -0,0 +1,12 @@
+name = input("Enter Your Name - ")
+age = input("Enter Your Age - ")
+
+print("Hi! my Name is", name, ", I am", age, "Years Old.")
+
+print("This line 'print()' is a function",)
+
+def myFun(a,b,c):
+  x = a + b * c
+  print(x)
+
+myFun(5,7,3)
